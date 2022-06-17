8c02418e93fa60a2f8ec8a4978a047ea5c98d25e
vanishettar <vanishettar12@gmail.com>
2022-06-17 20:11:36 +0530

    Reflection Day1

---
+++ Reflection-Automation/src/com/sgtesting/reflection5/TestRunner.java
@@ -0,0 +1,67 @@
+package com.sgtesting.reflection5;
+
+import java.io.FileInputStream;
+import java.lang.reflect.Method;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class TestRunner {
+
+	public static void main(String[] args) {
+		executeTests();
+
+	}
+	
+	private static void executeTests()
+	{
+		FileInputStream fin=null;
+		Workbook wb=null;
+		Sheet sh=null;
+		Row row=null;
+		Cell cell=null;
+		try
+		{
+			fin=new FileInputStream("E:\\CurrentWorkSpace\\VaniMonty101\\Reflection-Automation\\DataFiles\\data_Runner.xlsx");
+			wb=new XSSFWorkbook(fin);
+			sh=wb.getSheet("Sheet1");
+			int rc=sh.getPhysicalNumberOfRows();
+			for(int r=1;r<rc;r++)
+			{
+				row=sh.getRow(r);
+				cell=row.getCell(0);
+				String methodname=cell.getStringCellValue();
+				
+				cell=row.getCell(1);
+				String pkgclassname=cell.getStringCellValue();
+				
+				System.out.println(methodname+" ---> "+pkgclassname);
+				
+				Class clsObject=Class.forName(pkgclassname);
+				Object obj=clsObject.getConstructor().newInstance();
+				
+				Method method=obj.getClass().getMethod(methodname);
+				method.invoke(obj);
+				
+			}
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				wb.close();
+				fin.close();
+			}catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ Reflection-Automation/src/com/sgtesting/reflection6/TestRunner.java
@@ -0,0 +1,75 @@
+package com.sgtesting.reflection6;
+
+import java.io.FileInputStream;
+import java.lang.reflect.Method;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class TestRunner {
+
+	public static void main(String[] args) {
+		executeTests();
+
+	}
+	
+	private static void executeTests()
+	{
+		FileInputStream fin=null;
+		Workbook wb=null;
+		Sheet sh=null;
+		Row row=null;
+		Cell cell=null;
+		try
+		{
+			fin=new FileInputStream("E:\\CurrentWorkSpace\\VaniMonty101\\Reflection-Automation\\DataFiles\\data_Runner_New.xlsx");
+			wb=new XSSFWorkbook(fin);
+			int sc=wb.getNumberOfSheets();
+			for(int s=0;s<sc;s++)
+			{
+				String sname=wb.getSheetName(s);
+				System.out.println(sname);
+				sh=wb.getSheet(sname);
+				int rc=sh.getPhysicalNumberOfRows();
+				for(int r=1;r<rc;r++)
+				{
+					row=sh.getRow(r);
+					cell=row.getCell(0);
+					String methodname=cell.getStringCellValue();
+					
+					cell=row.getCell(1);
+					String pkgclassname=cell.getStringCellValue();
+					
+					System.out.println(methodname+" ---> "+pkgclassname);
+					
+					Class clsObject=Class.forName(pkgclassname);
+					Object obj=clsObject.getConstructor().newInstance();
+					
+					Method method=obj.getClass().getMethod(methodname);
+					method.invoke(obj);
+					
+				}
+			}
+			
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				wb.close();
+				fin.close();
+			}catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
